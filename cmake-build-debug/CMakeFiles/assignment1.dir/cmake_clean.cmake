286e16fc0b2b543dd30181fce0317ab426b33464
Yunus Melih Gozutok <ymgozutok@gmail.com>
2018-10-28 02:21:16 +0300

    hi

---
+++ cmake-build-debug/CMakeFiles/assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1.dir/main.c.obj"
+  "assignment1.pdb"
+  "assignment1.exe"
+  "assignment1.exe.manifest"
+  "libassignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
