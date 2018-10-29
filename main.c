a1e59a252f97f55edcd7125e9314e3b70c06a46e
Yunus Melih Gozutok <ymgozutok@gmail.com>
2018-10-30 02:49:07 +0300

    changing hello ammsterdam message

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <malloc.h>
 
 int main() {
-    double am = 3.12;
-    printf("variable: %d", am);
+
+    readMap();
+
+}
+
+void readMap(){
+    int i;
+    int j;
+
+
+
+    FILE *fp;
+    fp=fopen("mapmatrix1.txt", "r");
+    if (fp == NULL) {
+        printf("File not found");
+        exit(1);
+    }
+    int c, count;
+
+    count = 0;
+    for( ;; )
+    {
+        c = fgetc( fp );
+        if( c == EOF || c == '\n' )
+            break;
+        ++count;
+    }
+
+    int n = count/2+1;
+
+
+  // fclose(fp);
+
+/*matrix*/
+    float **map=malloc(n*sizeof(float));
+    for(i=0;i<n;++i)
+        map[i]=malloc(4*sizeof(float));
+
+
+
 }
